5dd65c7721e8c4f7634c12505fa8e15763d4e77b
Moha-Sameh <ms.slama01@hotmail.com>
2021-03-12 23:37:21 +0200

    Add Navbar

---
+++ apple-tv/src/Styles/GlobalStyling.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+
+main {
+    display: block;
+}
+
+body {
+    font-size: 17px;
+    line-height: 1.47059;
+    font-weight: 400;
+    letter-spacing: -.022em;
+    font-family: "SF Pro Text","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+    color: #1d1d1f;
+    font-style: normal;
+}
+:root {
+    --tile-bg: #f9f9f9;
+}
+
+html {
+    font-family: "SF Pro Text","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+    font-size: 106.25%;
+    quotes: "“" "”";
+}
+
+
+`;
+
+export default GlobalStyle;
